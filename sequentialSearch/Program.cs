52d5645e943894f3d5b6b729b71f56eae46e22d5
Sakhile-Msibi <Sakhile@codeplex.co.za>
2022-10-26 18:04:07 +0200

    Add project files.

---
+++ sequentialSearch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sequentialSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 31, 41, 59, 26, 41, 58 };
+            Console.WriteLine(sequentialSearch(A, 59));
+        }
+
+        public static int sequentialSearch(int[] numbers, int K)
+        {
+            int i = 0;
+            int n = numbers.Length;
+            while (i < n && numbers[i] != K)
+            {
+                i++;
+            }
+            if (i < n)
+                return i;
+            else
+                return -1;
+        }
+    }
+}
